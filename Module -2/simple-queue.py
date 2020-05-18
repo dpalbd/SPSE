97ebc89fe96fad8f32b86fd30bfef6a0b4e69cad
dpalbd <dpalbd@gmail.com>
2020-05-18 17:45:15 +0600

    Create simple-queue.py

---
+++ Module -2/simple-queue.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import threading
+import Queue
+import time
+
+
+class WorkerThread(threading.Thread):
+
+        def __init__(self, queue):
+                threading.Thread.__init__(self)
+                self.queue = queue
+
+        def run(self):
+                print "In WorkerThread"
+
+                while True:
+                        counter = self.queue.get()
+                        print "Ordered to sleep for %d seconds!" %counter
+                        time.sleep(counter)
+                        print "Finished Sleeping for %d seconds"%counter
+                        self.queue.task_done()
+
+
+
+#queue to fetch work from
+queue = Queue.Queue()
+
+for i in range(10):
+        print "Creating WorkerThread : %d" %i
+
+        #give the queue to the Workerthread
+        worker = WorkerThread(queue)
+        worker.setDaemon(True)
+        worker.start()
+        print "WorkerThread %d Created!"%i
+
+
+for j in range(10):
+        queue.put(j)
+
+queue.join()
+print "All tasks are over!"
