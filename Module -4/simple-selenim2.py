d4c2f63f5e2d0e1f4259cc4a2aefe3aa93e1cb85
dpalbd <dpalbd@gmail.com>
2020-05-25 21:22:21 +0600

    Create simple-selenim2.py

---
+++ Module -4/simple-selenim2.py
@@ -0,0 +1,20 @@
+#https://stackoverflow.com/questions/46753393/how-to-make-firefox-headless-programmatically-in-selenium-with-python
+#https://stackoverflow.com/questions/53657215/running-selenium-with-headless-chrome-webdriver
+
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+
+
+cap = DesiredCapabilities().FIREFOX
+cap["marionette"] = False
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path='/usr/local/bin/geckodriver')
+driver.get("http://google.com/")
+print ("Headless Firefox Initialized")
+
